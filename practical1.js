668da6a24229177823e401167efe148c55c1fb32
ShreyanshVala <125125984+ShreyanshVala@users.noreply.github.com>
2026-04-10 10:25:48 +0530

    first commit

---
+++ practical1.js
@@ -0,0 +1,11 @@
+let name = "ravi";
+let age = 23;
+let isStudent = true;
+let value = null;
+let data;
+
+console.log("Name:", name, "|Type:", typeof name);
+console.log("Age:", age, "|Type:", typeof age);
+console.log("Is Student:", isStudent, "|Type:", typeof isStudent);
+(console.log("Check value:", value), "|Type of :", typeof value);
+console.log("Value Assign:", data, "|Type:", typeof data);
